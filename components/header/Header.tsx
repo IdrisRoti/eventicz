161f425a907740805c315635e1afd98ae0644aa3
IdrisRoti <idrisomisakin@gmail.com>
2025-02-11 19:02:23 +0100

    style: restrict header width on mobile

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,7 +3,7 @@ import Navlinks from './Navlinks'
 
 const Header = () => {
   return (
-    <header className='max-w-[75rem] mx-auto h-[4.75rem] border border-primary fixed top-6 left-0 right-0 rounded-3xl backdrop-blur-[4] bg-[#05252C66]'>
+    <header className='w-[90vw] max-w-[75rem] mx-auto h-[4.75rem] border border-primary fixed top-6 left-0 right-0 rounded-3xl backdrop-blur-sm bg-[#05252C66] z-[99]'>
         <div className="flex items-center justify-between py-3 px-4 h-full">
             <Logo />
             <Navlinks />
